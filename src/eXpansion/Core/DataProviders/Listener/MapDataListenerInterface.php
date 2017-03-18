0d748a901f974daabaabb436b2681a1be2cb95b2
oliverde8 <oliverde8@gmail.com>
2017-03-18 11:42:56 +0100

    Added Map storage.

---
+++ src/eXpansion/Core/DataProviders/Listener/MapDataListenerInterface.php
@@ -0,0 +1,19 @@
+<?php
+namespace eXpansion\Core\DataProviders\Listener;
+
+use eXpansion\Core\DataProviders\PlayerDataProvider;
+use eXpansion\Core\Storage\Data\Player;
+
+/**
+ * Interface PlayerDataListenerInterface for plugins using the MapDataProvider data provider.
+ *
+ * @see PlayerDataProvider
+ */
+interface MapDataListenerInterface
+{
+    public function onMapListModified($oldMaps, $currentMapUid, $nextMapUid);
+
+    public function onExpansionMapChange($currentMap, $reviousMap);
+
+    public function onExpansionNextMapChange($nextMap, $previousNextMap);
+}
