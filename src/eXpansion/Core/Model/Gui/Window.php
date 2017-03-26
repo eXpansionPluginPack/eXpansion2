3ff6253765a70cec976d23fbd114080917e28673
reaby <petri.jarvisalo@gmail.com>
2017-03-26 18:18:14 +0300

    Adding Window

--- src/eXpansion/Core/Model/Gui/Window.php
+++ src/eXpansion/Core/Model/Gui/Window.php
@@ -3,7 +3,14 @@
 namespace eXpansion\Core\Model\Gui;
 
 use eXpansion\Core\Model\UserGroups\Group;
+use Manialib\Manialink\Elements\Frame;
+use Manialib\Manialink\Elements\Label;
 use Manialib\Manialink\Elements\Quad;
+use Manialib\Manialink\Elements\Script;
+use Manialib\Manialink\Elements\Stylesheet;
+use Manialib\Manialink\Styles\Bgs1;
+use Manialib\XML\Fragment;
+use Manialib\XML\Rendering\Renderer;
 
 class Window extends Manialink
 {
@@ -14,11 +21,153 @@ class Window extends Manialink
     {
         parent::__construct($group, $name, $sizeX, $sizeY, $posX, $posY);
 
+        $titleHeight = 5.5;
+        $closeButtonWidth = 9.5;
+        $closeButtonHighliteColor = "D30707ee";
+        $titlebarColor = "3afe";
+
         $ml = new \Manialib\Manialink\Elements\Manialink();
         $ml->setVersion(3);
         $ml->setName($name);
+        $ml->setAttribute("id", $this->getId());
+        $window = Frame::create()->setId("Window")->setPosn($posX, $posY)->appendTo($ml);
 
-        Quad::create()->setSizen($sizeX, $sizeY)->setPosn($posX, $posY)->setBgcolor("0008")->appendTo($ml);
+        // titlebar text
+        Label::create()
+            ->setPosn(3, -$titleHeight / 2)
+            ->setAlign("left", "center2")
+            ->setText($name)
+            ->setTextcolor("fff")
+            ->setTextsize(2)
+            ->setAttribute("textfont", "RajdhaniMono")
+            ->appendTo($window);
+
+
+        // titlebar
+        Quad::create()
+            ->setSizen($sizeX, 0.33)
+            ->setPosn(0, -$titleHeight)
+            ->setBgcolor("fff")
+            ->appendTo($window);
+
+        Quad::create()
+            ->setSizen($sizeX / 4, 0.5)
+            ->setAlign("left", "bottom")
+            ->setPosn(0, -$titleHeight)
+            ->setBgcolor("fff")
+            ->appendTo($window);
+
+
+        Quad::create()
+            ->setSizen($sizeX - $closeButtonWidth, $titleHeight)
+            ->setBgcolor($titlebarColor)
+            ->setId("title")
+            ->setScriptevents()
+            ->appendTo($window);
+
+        Label::create()
+            ->setId("Close")
+            ->setSizen($closeButtonWidth, $titleHeight)
+            ->setPosn($sizeX - $closeButtonWidth + ($closeButtonWidth / 2), -$titleHeight / 2)
+            ->setAlign("center", "center2")
+            ->setText("✖")
+            ->setScriptevents()
+            ->setTextcolor("fff")
+            ->setFocusareacolor1($titlebarColor)
+            ->setFocusareacolor2($closeButtonHighliteColor)
+            ->setTextSize(2)
+            ->setAttribute("textfont", "OswaldMono")
+            ->appendTo($window);
+
+        //body
+        Quad::create()
+            ->setSizen($sizeX, ($sizeY - $titleHeight))
+            ->setPosn(0, -$titleHeight)
+            ->setStyle("Bgs1:BgWindow1")
+            ->appendTo($window);
+
+        Quad::create()
+            ->setStyle("Bgs1InRace:BgButtonShadow")
+            ->setSizen($sizeX + 10, $sizeY + 10)
+            ->setPosn(-5, 5)
+            ->appendTo($window);
+
+
+        $script = /** @lang textmate */
+            <<<'EOD'
+
+#Include "AnimLib" as AL
+
+main () {
+
+    declare CMlFrame Window <=> (Page.GetFirstChild("Window") as CMlFrame);
+    declare CMlQuad Titlebar <=> (Page.GetFirstChild("title") as CMlQuad);
+    declare moveWindow = False;
+    declare closeWindow = False;
+    declare openWindow = False;
+    declare Vec2 Offset = <0.0, 0.0	>;
+    declare Real zIndex = 0.;
+    declare Boolean MoveWindow = False;
+    declare Integer lastAction = Now;
+	
+    while(True) {
+		yield;
+		
+        if (openWindow) {
+            AnimMgr.Add(Window, "<frame scale=\"1.\" />", 200, CAnimManager::EAnimManagerEasing::SineIn);   
+       	}
+        
+        if (closeWindow) {     	 
+         	AnimMgr.Add(Window, "<frame scale=\"0.\" />", 200, CAnimManager::EAnimManagerEasing::SineOut);   
+        }
+
+        if (openWindow && lastAction + 200 <= Now ) {
+            openWindow = False;
+            lastAction = 0;
+        }
+
+	    if (closeWindow && lastAction + 200 <= Now ) {
+            closeWindow = False;
+	        continue;
+	    }
+	
+	    if (MoveWindow) {
+            Window.RelativePosition_V3.X = MouseX + Offset.X;
+	        Window.RelativePosition_V3.Y = MouseY + Offset.Y;
+	    }
+	
+	    if (PendingEvents.count != 0) {
+            foreach (Event in PendingEvents) {
+                if ( (Event.Type == CMlEvent::Type::MouseClick && Event.ControlId == "Close")  || (Event.Type == CMlEvent::Type::KeyPress && Event.KeyCode == 35) ) {
+                    closeWindow = True;
+                    lastAction = Now;
+                    log("close");
+                }
+                
+                if ( (Event.Type == CMlEvent::Type::MouseClick && Event.ControlId == "Open") ) {
+                    log("open");
+                    lastAction = Now;
+                    openWindow = True;
+                }
+            }
+	    }	
+	
+	    if (MouseLeftButton == True) {
+            foreach (Event in PendingEvents) {
+                if (Event.Type == CMlEvent::Type::MouseClick && Event.ControlId == "title")  {		
+                Offset = <Window.RelativePosition_V3.X - MouseX, Window.RelativePosition_V3.Y - MouseY>;
+                MoveWindow = True;
+                 }
+            }
+        } else {
+            MoveWindow = False;
+        }
+
+    }
+
+}
+EOD;
+        Script::create()->setNodeValue($script)->appendTo($ml);
 
         $this->manialink = $ml;
     }
@@ -26,7 +175,9 @@ class Window extends Manialink
 
     public function getXml()
     {
-        return (string)$this->manialink;
+        $renderer = new Renderer();
+        $renderer->setRoot($this->manialink);
+        return $renderer->getXML($this->manialink) . "\n";
     }
 
 }
