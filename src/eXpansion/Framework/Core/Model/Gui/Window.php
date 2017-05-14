dfe3741f3bab93800f41a42e0c5545fb2b749dbf
oliverde8 <oliverde8@gmail.com>
2017-05-14 12:04:26 +0200

    #45 - Switched to FML for manialink creation.

--- src/eXpansion/Framework/Core/Model/Gui/Window.php
+++ src/eXpansion/Framework/Core/Model/Gui/Window.php
@@ -4,17 +4,25 @@ namespace eXpansion\Framework\Core\Model\Gui;
 
 use eXpansion\Framework\Core\Exceptions\Gui\MissingCloseActionException;
 use eXpansion\Framework\Core\Model\UserGroups\Group;
-use Manialib\Manialink\Elements\Frame;
-use Manialib\Manialink\Elements\Label;
-use Manialib\Manialink\Elements\Quad;
-use Manialib\Manialink\Elements\Script;
-use Manialib\XML\Rendering\Renderer;
+use FML\Controls\Frame;
+use FML\Controls\Label;
+use FML\Controls\Quad;
+use FML\Controls\Quads\Quad_Bgs1;
+use FML\Controls\Quads\Quad_Bgs1InRace;
+use FML\Elements\Format;
+use FML\Types\Container;
+use FML\Types\Renderable;
 
-class Window extends Manialink
+class Window extends Manialink implements Container
 {
+    /** @var \FML\ManiaLink  */
     protected $manialink;
 
+    /** @var Label  */
     protected $closeButton;
+
+    /** @var Frame  */
+    protected $contentFrame;
 
     public function __construct(
         Group $group,
@@ -28,168 +36,225 @@ class Window extends Manialink
 
         $titleHeight = 5.5;
         $closeButtonWidth = 9.5;
-        $closeButtonHighliteColor = "D30707ee";
         $titlebarColor = "3afe";
 
-        $ml = new \Manialib\Manialink\Elements\Manialink();
-        $ml->setVersion(3);
-        $ml->setAttribute('id', $this->getId());
-        $ml->setName($name);
-        $window = Frame::create()->setId("Window")->setPosn($posX, $posY)->appendTo($ml);
+        // Manialink containing everything
+        $this->manialink = new \FML\ManiaLink();
+        $this->manialink->setId($this->getId())
+                        ->setName($name)
+                        ->setVersion(\FML\ManiaLink::VERSION_3);
+        $windowFrame = new Frame('Window');
+        $windowFrame->setPosition($posX, $posY);
+        $this->manialink->addChild($windowFrame);
 
-        // titlebar text
-        Label::create()
-            ->setPosn(3, -$titleHeight / 2)
-            ->setAlign("left", "center2")
+        // Title bar & title.
+        $titleLabel = new Label();
+        $titleLabel->setPosition(3, -$titleHeight/3 - 1)
+            ->setAlign(Label::LEFT, Label::CENTER2)
             ->setText($name)
-            ->setTextcolor("fff")
-            ->setTextsize(2)
-            ->setAttribute("textfont", "RajdhaniMono")
-            ->appendTo($window);
-
-
-        // titlebar
-        Quad::create()
-            ->setSizen($sizeX, 0.33)
-            ->setPosn(0, -$titleHeight)
-            ->setBgcolor("fff")
-            ->appendTo($window);
-
-        Quad::create()
-            ->setSizen($sizeX / 4, 0.5)
-            ->setAlign("left", "bottom")
-            ->setPosn(0, -$titleHeight)
-            ->setBgcolor("fff")
-            ->appendTo($window);
-
-        Quad::create()
-            ->setSizen($sizeX - $closeButtonWidth, $titleHeight)
-            ->setBgcolor($titlebarColor)
-            ->setId("Title")
-            ->setScriptevents()
-            ->appendTo($window);
-
-        $this->closeButton = Label::create()
-            ->setId("Close")
-            ->setSizen($closeButtonWidth, $titleHeight)
-            ->setPosn($sizeX - $closeButtonWidth + ($closeButtonWidth / 2), -$titleHeight / 2)
-            ->setAlign("center", "center2")
-            ->setText("✖")
-            ->setScriptevents()
-            ->setTextcolor("fff")
-            ->setFocusareacolor1($titlebarColor)
-            ->setFocusareacolor2($closeButtonHighliteColor)
+            ->setTextColor('fff')
             ->setTextSize(2)
-            ->setAttribute("textfont", "OswaldMono")
-            ->appendTo($window);
+            ->setTextFont('RajdhaniMono');
+        $windowFrame->addChild($titleLabel);
+
+        $titleBar = new Quad();
+        $titleBar->setSize($sizeX, 0.33)
+            ->setAlign(null, null) // Fix issue with FML's default values.
+            ->setPosition(0, -$titleHeight)
+            ->setBackgroundColor('fff');
+        $windowFrame->addChild($titleBar);
+
+        $titleBar = new Quad();
+        $titleBar->setSize($sizeX / 4, 0.5)
+            ->setAlign(null, null) // Fix issue with FML's default values.
+            ->setPosition(0, -$titleHeight)
+            ->setBackgroundColor('fff');
+        $windowFrame->addChild($titleBar);
+
+        $titleBar = new Quad('Title"');
+        $titleBar->setSize($sizeX - $closeButtonWidth, $titleHeight)
+            ->setAlign(null, null) // Fix issue with FML's default values.
+            ->setBackgroundColor($titlebarColor)
+            ->setScriptEvents(true);
+        $windowFrame->addChild($titleBar);
+
+        $this->closeButton = new Label('Close');
+        $this->closeButton->setSize($closeButtonWidth, $titleHeight)
+            ->setPosition($sizeX - $closeButtonWidth + ($closeButtonWidth / 2), -$titleHeight / 2)
+            ->setAlign(Label::CENTER, Label::CENTER2)
+            ->setText("✖")
+            ->setTextColor('fff')
+            ->setTextSize(2)
+            ->setTextFont('OswaldMono')
+            ->setScriptEvents(true)
+            ->setAreaFocusColor($titlebarColor);
+        $windowFrame->addChild($this->closeButton);
 
         //body
-        Quad::create()
-            ->setSizen($sizeX, ($sizeY - $titleHeight))
-            ->setPosn(0, -$titleHeight)
-            ->setStyle("Bgs1:BgWindow3")
-            ->appendTo($window);
+        $body = new Quad_Bgs1();
+        $body->setSize($sizeX, $sizeY - $titleHeight)
+            ->setAlign(null, null) // Fix issue with FML's default values.
+            ->setPosition(0, -$titleHeight)
+            ->setSubStyle(Quad_Bgs1::SUBSTYLE_BgWindow3);
+        $windowFrame->addChild($body);
 
-        Quad::create()
-            ->setStyle("Bgs1InRace:BgButtonShadow")
-            ->setSizen($sizeX + 10, $sizeY + 10)
-            ->setPosn(-5, 5)
-            ->appendTo($window);
+        $body = new Quad_Bgs1InRace();
+        $body->setSize($sizeX + 10, $sizeY + 10)
+            ->setAlign(null, null) // Fix issue with FML's default values.
+            ->setPosition(-5, 5)
+            ->setSubStyle(Quad_Bgs1InRace::SUBSTYLE_BgButtonShadow);
+        $windowFrame->addChild($body);
 
+        // Frame to handle the content of the window.
+        $this->contentFrame = new Frame();
+        $this->contentFrame->setPosition(2, -$titleHeight - 2);
+        $windowFrame->addChild($this->contentFrame);
 
-        $script = /** @lang textmate */
-            <<<'EOD'
-
-#Include "AnimLib" as AL
-
-main () {
-
-    declare CMlFrame Window <=> (Page.GetFirstChild("Window") as CMlFrame);
-    declare CMlQuad Titlebar <=> (Page.GetFirstChild("Title") as CMlQuad);
-    declare CMlLabel CloseButton <=> (Page.GetFirstChild("Close") as CMlLabel);
-    declare moveWindow = False;
-    declare closeWindow = False;
-    declare openWindow = False;
-    declare Vec2 Offset = <0.0, 0.0	>;
-    declare Real zIndex = 0.;
-    declare Boolean MoveWindow = False;
-    declare Integer lastAction = Now;
-	
-    while(True) {
-		yield;
-		
-        if (openWindow) {
-            AnimMgr.Add(Window, "<frame scale=\"1.\" />", 200, CAnimManager::EAnimManagerEasing::SineIn);   
-       	}
-        
-        if (closeWindow) {     	 
-         	AnimMgr.Add(Window, "<frame scale=\"0.\" />", 200, CAnimManager::EAnimManagerEasing::SineOut);   
-        }
-
-        if (openWindow && lastAction + 200 <= Now ) {
-            openWindow = False;
-            lastAction = 0;
-        }
-
-	    if (closeWindow && lastAction + 200 <= Now ) {
-            closeWindow = False;
-            TriggerPageAction(CloseButton.DataAttributeGet("action"));
-	        continue;
-	    }
-	
-	    if (MoveWindow) {
-            Window.RelativePosition_V3.X = MouseX + Offset.X;
-	        Window.RelativePosition_V3.Y = MouseY + Offset.Y;
-	    }
-	
-	    if (PendingEvents.count != 0) {
-            foreach (Event in PendingEvents) {
-                if ( (Event.Type == CMlEvent::Type::MouseClick && Event.ControlId == "Close")  || 
-                   (Event.Type == CMlEvent::Type::KeyPress && Event.KeyCode == 35) ) {
-                    closeWindow = True;
-                    lastAction = Now;                  
-                }
-                
-                if ( (Event.Type == CMlEvent::Type::MouseClick && Event.ControlId == "Open") ) {
-                    lastAction = Now;
-                    openWindow = True;
-                }
-            }
-	    }	
-	
-	    if (MouseLeftButton == True) {
-            foreach (Event in PendingEvents) {
-                if (Event.Type == CMlEvent::Type::MouseClick && Event.ControlId == "Title")  {		
-                    Offset = <Window.RelativePosition_V3.X - MouseX, Window.RelativePosition_V3.Y - MouseY>;
-                    MoveWindow = True;
-                }
-            }
-        } else {
-            MoveWindow = False;
-        }
-
+        // TODO put back maniascript to move windows.
     }
 
-}
-EOD;
-        Script::create()->setNodeValue($script)->appendTo($ml);
-        $this->manialink = $ml;
-    }
-
+    /**
+     * Set action to close the window.
+     *
+     * @param $actionId
+     */
     public function setCloseAction($actionId)
     {
-        $this->closeButton->setAttribute('data-action', $actionId);
+        $this->closeButton->setDataAttributes(['action' => $actionId]);
     }
 
+    /**
+     * @inheritdoc
+     */
     public function getXml()
     {
-        if (empty($this->closeButton->getAttribute('data-action')))
+        if (empty($this->closeButton->getDataAttribute('action')))
         {
             throw new MissingCloseActionException("Close action is missing for window. Check if you are using the proper factory.");
         }
 
-        $renderer = new Renderer();
-        return $renderer->getXML($this->manialink);
+        echo $this->manialink->__toString();
+
+        return $this->manialink->__toString();
     }
 
+    /**
+     * Get the children
+     *
+     * @api
+     * @return Renderable[]
+     */
+    public function getChildren()
+    {
+        return $this->contentFrame->getChildren();
+    }
+
+    /**
+     * Add a new child
+     *
+     * @api
+     *
+     * @param Renderable $child Child Control to add
+     *
+     * @return static
+     */
+    public function addChild(Renderable $child)
+    {
+        $this->contentFrame->addChild($child);
+
+        return $this;
+    }
+
+    /**
+     * Add a new child
+     *
+     * @api
+     *
+     * @param Renderable $child Child Control to add
+     *
+     * @return static
+     * @deprecated Use addChild()
+     * @see        Container::addChild()
+     */
+    public function add(Renderable $child)
+    {
+        $this->contentFrame->addChild($child);
+
+        return $this;
+    }
+
+    /**
+     * Add new children
+     *
+     * @api
+     *
+     * @param Renderable[] $children Child Controls to add
+     *
+     * @return static
+     */
+    public function addChildren(array $children)
+    {
+        $this->contentFrame->addChildren($children);
+
+        return $this;
+    }
+
+    /**
+     * Remove all children
+     *
+     * @api
+     * @return static
+     */
+    public function removeAllChildren()
+    {
+        $this->contentFrame->removeAllChildren();
+
+        return $this;
+    }
+
+    /**
+     * Remove all children
+     *
+     * @api
+     * @return static
+     * @deprecated Use removeAllChildren()
+     * @see        Container::removeAllChildren()
+     */
+    public function removeChildren()
+    {
+        $this->contentFrame->removeAllChildren();
+
+        return $this;
+    }
+
+    /**
+     * Get the Format
+     *
+     * @api
+     *
+     * @param bool $createIfEmpty If the format should be created if it doesn't exist yet
+     *
+     * @return Format
+     * @deprecated Use Style
+     * @see        Style
+     */
+    public function getFormat($createIfEmpty = true)
+    {
+        return $this->contentFrame->getFormat($createIfEmpty);
+    }
+
+    /**
+     * Set the Format
+     *
+     * @api
+     *
+     * @param Format $format New Format
+     *
+     * @return static
+     * @deprecated Use Style
+     * @see        Style
+     */
+    public function setFormat(Format $format = null) {
+        return $this->contentFrame->setFormat($format);
+    }
 }
