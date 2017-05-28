8eacd8b6adb4d0d01a0a2c98ce09c4c065a2d50a
reaby <petri.jarvisalo@gmail.com>
2017-05-28 23:54:52 +0300

    Adding menu content

--- src/eXpansion/Bundle/Menu/Plugins/Gui/MenuFactory.php
+++ src/eXpansion/Bundle/Menu/Plugins/Gui/MenuFactory.php
@@ -7,7 +7,9 @@ use eXpansion\Framework\Core\Model\Gui\ManialinkInterface;
 use eXpansion\Framework\Core\Model\Gui\ManiaScriptFactory;
 use eXpansion\Framework\Core\Model\Gui\Widget;
 use eXpansion\Framework\Core\Plugins\Gui\WidgetFactory;
+use FML\Controls\Frame;
 use FML\Controls\Label;
+use FML\Controls\Quad;
 
 
 /**
@@ -45,10 +47,112 @@ class MenuFactory extends WidgetFactory
      */
     protected function createContent(ManialinkInterface $manialink)
     {
-        $label = Label::create();
-        $label->setText('TEST');
+        /* Button frame first */
 
-        $manialink->addChild($label);
+        $label = Label::create("open");
+        $label->setText("Open")
+            ->setAreaFocusColor("5ff")
+            ->setAreaColor("3af")
+            ->setPosition(100, 60)
+            ->setScriptEvents(true);
+       // $manialink->addChild($label);
+
+        $btnFrame = Frame::create("buttons");
+        $btnFrame->setZ(101)->setPosition(0, 30);
+
+        $y = 0;
+        $baseLabel = Label::create();
+        $baseLabel->setAreaColor("0000")
+            ->setAreaFocusColor("0000")
+            ->setTextColor("FFF")
+            ->setAlign("center", "center2")
+            ->addClass("button")
+            ->setScriptEvents(true);
+
+        $label = clone $baseLabel;
+        $label->setPosition(0, $y -= 8)
+            ->setText("Help")
+            ->setTextPrefix(" ")
+            ->setDataAttributes(["action" => "!help"]);
+        $btnFrame->addChild($label);
+
+        $label = clone $baseLabel;
+        $label->setPosition(0, $y -= 10)
+            ->setText("Show Profile")
+            ->setTextPrefix(" ")
+            ->setDataAttributes(["action" => "!profile"]);
+        $btnFrame->addChild($label);
+
+        $label = clone $baseLabel;
+        $label->setPosition(0, $y -= 8)
+            ->setText("Spectate")
+            ->setTextPrefix("")
+            ->setDataAttributes(["action" => "!spec"]);
+        $btnFrame->addChild($label);
+
+        $label = clone $baseLabel;
+        $label->setPosition(0, $y -= 8)
+            ->setText("Return to game")
+            ->setTextPrefix(" ")
+            ->setDataAttributes(["action" => "!close"]);
+        $btnFrame->addChild($label);
+
+
+        $label = clone $baseLabel;
+        $label->setPosition(0, $y -= 12)
+            ->setText("Return to main menu")
+            ->setTextPrefix("🏁 ")
+            ->setDataAttributes(["action" => "!quit"]);
+        $btnFrame->addChild($label);
+
+
+
+        $bgFrame = Frame::create("background");
+        $bgFrame->setZ(100);
+
+        $baseLabel = Label::create();
+        $baseLabel->setAreaColor("0000")
+            ->setAreaFocusColor("0000")
+            ->setTextColor("FFF")
+            ->setAlign("center", "center2")
+            ->addClass("bg");
+
+
+        $label = clone $baseLabel;
+        $label->setText("")
+            ->setTextSize(16)
+            ->setPosition(0, 50)
+            ->setSize(32, 32);
+        $bgFrame->addChild($label);
+
+        $label = clone $baseLabel;
+        $label->setText("Server Menu")
+            ->setTextSize(8)
+            ->setPosition(0, 35)
+            ->setTextFont('Oswald');
+        $bgFrame->addChild($label);
+
+
+        $quad = Quad::create();
+        $quad->addClass("bg")
+            ->setPosition(0, 28)
+            ->setSize(100, 0.5)
+            ->setAlign("center", "center")
+            ->setBackgroundColor("fff");
+        $bgFrame->addChild($quad);
+
+        $quad = Quad::create();
+        $quad->addClass("bg")
+            ->setPosition(0, 0)
+            ->setSize(322, 182)
+            ->setAlign("center", "center")
+            ->setStyles("Bgs1", "BgDialogBlur");
+        $bgFrame->addChild($quad);
+
+
+
+        $manialink->addChild($btnFrame);
+        $manialink->addChild($bgFrame);
 
         $manialink->addChild($this->menuScriptFactory->createScript([]));
     }
