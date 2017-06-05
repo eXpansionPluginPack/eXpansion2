fe4ed965abab388dae36f6aa677013e3a39ad5ac
oliverde8 <oliverde8@gmail.com>
2017-06-05 11:39:27 +0200

    #60 Added Local Records bundle with DB structure & Record Handler service.

---
+++ src/eXpansion/Bundle/LocalRecords/Plugins/BaseRecords.php
@@ -0,0 +1,199 @@
+<?php
+
+namespace eXpansion\Bundle\LocalRecords\Plugins;
+
+use eXpansion\Bundle\LocalRecords\Services\RecordHandler;
+use eXpansion\Bundle\LocalRecords\Services\RecordHandlerFactory;
+use eXpansion\Framework\Core\DataProviders\Listener\PlayerDataListenerInterface;
+use eXpansion\Framework\Core\Model\UserGroups\Group;
+use eXpansion\Framework\Core\Plugins\StatusAwarePluginInterface;
+use eXpansion\Framework\Core\Storage\Data\Player;
+use eXpansion\Framework\Core\Storage\MapStorage;
+use eXpansion\Framework\GameManiaplanet\DataProviders\Listener\BaseDataListenerInterface as MpBaseDataListenerInterface;
+use eXpansion\Framework\GameTrackmania\DataProviders\Listener\BaseDataListenerInterface as TmBaseDataListenerInterface;
+use Maniaplanet\DedicatedServer\Structures\Map;
+
+/**
+ * Class RaceRecords
+ *
+ * ADD status aware interface and load on activation.
+ *
+ * @package eXpansion\Bundle\LocalRecords\Plugins;
+ * @author  oliver de Cramer <oliverde8@gmail.com>
+ */
+class BaseRecords implements MpBaseDataListenerInterface, PlayerDataListenerInterface, StatusAwarePluginInterface
+{
+    /** @var  RecordHandler */
+    protected $recordsHandler;
+
+    /** @var Group */
+    protected $allPlayersGroup;
+
+    /** @var MapStorage */
+    protected $mapStorage;
+
+    /** @var string */
+    protected $eventPrefix;
+
+    /**
+     * RaceRecords constructor.
+     *
+     * @param RecordHandler $recordsHandler
+     * @param Group         $allPlayersGroup
+     * @param string        $eventPrefix
+     */
+    public function __construct(
+        RecordHandlerFactory $recordsHandlerFactory,
+        Group $allPlayersGroup,
+        MapStorage $mapStorage,
+        $eventPrefix
+    ) {
+        $this->recordsHandler = $recordsHandlerFactory->create();
+        $this->allPlayersGroup = $allPlayersGroup;
+        $this->mapStorage = $mapStorage;
+        $this->eventPrefix = $eventPrefix;
+    }
+
+    /**
+     * Set the status of the plugin
+     *
+     * @param boolean $status
+     *
+     * @return null
+     */
+    public function setStatus($status)
+    {
+        if ($status) {
+            $map = $this->mapStorage->getCurrentMap();
+
+            // Load firs X records for this map.
+            $this->recordsHandler->loadForMap($map->uId, $this->getNbLaps());
+
+            // Load time information for remaining players.
+            $this->recordsHandler->loadForPlayers($map->uId, $this->getNbLaps(), $this->allPlayersGroup->getLogins());
+        }
+    }
+
+    /**
+     * Callback sent when the "StartMap" section start.
+     *
+     * @param int     $count     Each time this section is played, this number is incremented by one
+     * @param int     $time      Server time when the callback was sent
+     * @param boolean $restarted true if the map was restarted, false otherwise
+     * @param Map     $map       Map started with.
+     *
+     * @return mixed
+     */
+    public function onStartMapStart($count, $time, $restarted, Map $map)
+    {
+        // Load firs X records for this map.
+        $this->recordsHandler->loadForMap($map->uId, $this->getNbLaps());
+
+        // Load time information for remaining players.
+        $this->recordsHandler->loadForPlayers($map->uId, $this->getNbLaps(), $this->allPlayersGroup->getLogins());
+    }
+
+    /**
+     * @param Player $player
+     */
+    public function onPlayerConnect(Player $player)
+    {
+        $this->recordsHandler->loadForPlayers($this->mapStorage->getCurrentMap()->uId, [1], [$player->getLogin()]);
+    }
+
+    /**
+     * Callback sent when the "EndMap" section end.
+     *
+     * @param int     $count     Each time this section is played, this number is incremented by one
+     * @param int     $time      Server time when the callback was sent
+     * @param boolean $restarted true if the map was restarted, false otherwise
+     * @param Map     $map       Map started with.
+     *
+     * @return mixed
+     */
+    public function onEndMapEnd($count, $time, $restarted, Map $map)
+    {
+        // Nothing to do
+    }
+
+    /**
+     * Callback sent when the "StartMatch" section start.
+     *
+     * @param int $count Each time this section is played, this number is incremented by one
+     * @param int $time  Server time when the callback was sent
+     *
+     * @return mixed
+     */
+    public function onStartMatchStart($count, $time)
+    {
+        // Nothing to do.
+    }
+
+    /**
+     * Callback sent when the "StartMatch" section end.
+     *
+     * @param int $count Each time this section is played, this number is incremented by one
+     * @param int $time  Server time when the callback was sent
+     *
+     * @return mixed
+     */
+    public function onStartMatchEnd($count, $time)
+    {
+        $this->recordsHandler->save();
+    }
+
+    /**
+     * Callback sent when the "StartMap" section end.
+     *
+     * @param int     $count     Each time this section is played, this number is incremented by one
+     * @param int     $time      Server time when the callback was sent
+     * @param boolean $restarted true if the map was restarted, false otherwise
+     * @param Map     $map       Map started with.
+     *
+     * @return mixed
+     */
+    public function onStartMapEnd($count, $time, $restarted, Map $map)
+    {
+        // Nothing to do.
+    }
+
+    /**
+     * Callback sent when the "EndMap" section start.
+     *
+     * @param int     $count     Each time this section is played, this number is incremented by one
+     * @param int     $time      Server time when the callback was sent
+     * @param boolean $restarted true if the map was restarted, false otherwise
+     * @param Map     $map       Map started with.
+     *
+     * @return mixed
+     */
+    public function onEndMapStart($count, $time, $restarted, Map $map)
+    {
+        // Nothing to do.
+    }
+
+    public function onPlayerDisconnect(Player $player, $disconnectionReason)
+    {
+        // Nothing to do.
+    }
+
+    public function onPlayerInfoChanged(Player $oldPlayer, Player $player)
+    {
+        // Nothing to do.
+    }
+
+    public function onPlayerAlliesChanged(Player $oldPlayer, Player $player)
+    {
+        // Nothing to do.
+    }
+
+    /**
+     * Get number of laps
+     *
+     * @return int
+     */
+    protected function getNbLaps()
+    {
+        return 1;
+    }
+}
---
+++ src/eXpansion/Bundle/LocalRecords/Services/RecordHandler.php
@@ -0,0 +1,312 @@
+<?php
+
+namespace eXpansion\Bundle\LocalRecords\Services;
+use eXpansion\Bundle\LocalRecords\Entity\Record;
+use eXpansion\Bundle\LocalRecords\Repository\RecordRepository;
+
+/**
+ * Class RecordHandler
+ *
+ * @package eXpansion\Bundle\LocalRecords\Model;
+ * @author  oliver de Cramer <oliverde8@gmail.com>
+ */
+class RecordHandler
+{
+    /**
+     * Available order logic for score.
+     */
+    const ORDER_ASC = "ASC";
+    const ORDER_DESC = "DESC";
+
+    /**
+     * List of event types
+     */
+    const EVENT_TYPE_FIRST_TIME = 'first_time';
+    const EVENT_TYPE_SAME_SCORE = 'same_score';
+    const EVENT_TYPE_SAME_POS =   'same_position';
+    const EVENT_TYPE_BETTER_POS = 'better_position';
+
+    /**
+     * List of data in the associative array returned.
+     */
+    const COL_EVENT = 'event';
+    const COL_RECORD = 'record';
+    const COL_OLD_RECORD = 'old_record';
+    const COL_POS = 'position';
+    const COL_OLD_POS = 'old_position';
+    const COL_RECORDS = 'records';
+
+    /** @var int */
+    protected $nbRecords;
+
+    /** @var string */
+    protected $ordering;
+
+    /** @var  RecordRepository */
+    protected $recordRepository;
+
+    /** @var Record[] */
+    protected $records;
+
+    /** @var Record[] */
+    protected $recordsPerPlayer;
+
+    /** @var int[] */
+    protected $positionPerPlayer;
+
+    /** @var int */
+    protected $currentNbLaps;
+
+    protected $currentMapUid;
+
+    /**
+     * RaceRecordHandler constructor.
+     *
+     * @param RecordRepository $recordRepository
+     * @param int $nbRecords
+     */
+    public function __construct(RecordRepository $recordRepository, $nbRecords, $ordering = self::ORDER_ASC)
+    {
+        $this->recordRepository = $recordRepository;
+        $this->nbRecords = $nbRecords;
+        $this->ordering = $ordering;
+    }
+
+    /**
+     * @return Record[]
+     */
+    public function getRecords()
+    {
+        return $this->records;
+    }
+
+    /**
+     * Load records for a certain map.
+     *
+     * @param $mapUid
+     * @param $nbLaps
+     */
+    public function loadForMap($mapUid, $nbLaps)
+    {
+        $this->recordsPerPlayer = [];
+        $this->positionPerPlayer = [];
+
+        $this->currentMapUid = $mapUid;
+        $this->currentNbLaps = $nbLaps;
+
+        $this->records = $this->recordRepository->findBy(
+            ['mapUid' => $mapUid, 'nbLaps' => $nbLaps],
+            ['score' => $this->getScoreOrdering()],
+            $this->nbRecords
+        );
+
+        $position = 1;
+        foreach ($this->records as $record)
+        {
+            $this->recordsPerPlayer[$record->getPlayerLogin()] = $record;
+            $this->positionPerPlayer[$record->getPlayerLogin()] = $position++;
+        }
+    }
+
+    /**
+     * Load records for certain players only.
+     *
+     * @param $mapUid
+     * @param $nbLaps
+     * @param $logins
+     */
+    public function loadForPlayers($mapUid, $nbLaps, $logins)
+    {
+        $logins = array_diff(array_keys($this->recordsPerPlayer), $logins);
+
+        if (!empty($logins)) {
+            $records = $this->recordRepository->findBy(
+                ['mapUid' => $mapUid, 'nbLaps' => $nbLaps, 'playerLogin' => $logins],
+                ['score' => $this->getScoreOrdering()],
+                $this->nbRecords
+            );
+
+            foreach ($records as $record) {
+                $this->recordsPerPlayer[$record->getPlayerLogin()] = $record;
+            }
+        }
+    }
+
+    /**
+     * Save all new records.
+     */
+    public function save()
+    {
+        $this->recordRepository->massSave($this->recordsPerPlayer);
+    }
+
+    /**
+     * Add a new record
+     *
+     * @param string $login
+     * @param int $score
+     * @param int[] $checkpoints
+     *
+     * @return array|null Data for the new records.
+     */
+    public function addRecord($login, $score, $checkpoints) {
+        $oldPosition = isset($this->positionPerPlayer[$login]) ? $this->positionPerPlayer[$login] : count($this->records) + 1;
+        $record = isset($this->recordsPerPlayer[$login]) ? $this->recordsPerPlayer[$login] : $this->getNewRecord($login);
+
+        $oldRecord = clone $record;
+        $this->updateRecordStats($record, $score);
+
+        if (empty($this->records)) {
+            $record->setScore($score);
+            $record->setDate(new \DateTime());
+            $record->setCheckpointTimes($checkpoints);
+
+            $this->records[0] = $record;
+            $this->positionPerPlayer[$record->getPlayerLogin()] = 1;
+            $this->recordsPerPlayer[$record->getPlayerLogin()] = $record;
+
+            return [
+                self::COL_EVENT => self::EVENT_TYPE_FIRST_TIME,
+                self::COL_RECORD => $record,
+                self::COL_RECORDS => $this->records,
+                self::COL_POS => 1,
+            ];
+        }
+
+        // Check if first time of this player.
+        $firstTime = is_null($record->getScore());
+
+        if ($firstTime || $this->compareNewScore($score, $record)) {
+            if ($score == $record->getScore()) {
+                return [
+                    self::COL_EVENT => self::EVENT_TYPE_SAME_SCORE,
+                    self::COL_RECORD => $record,
+                    self::COL_OLD_RECORD => $oldRecord,
+                    self::COL_RECORDS => $this->records
+                ];
+            }
+
+            $record->setScore($score);
+            $record->setDate(new \DateTime());
+            $record->setCheckpointTimes($checkpoints);
+
+            $betterRecordIndex = $oldPosition - 1;
+            $newPosition = $oldPosition;
+
+            echo "Better Record Index : $betterRecordIndex\n";
+
+            while ($betterRecordIndex >= 0 && $this->compareRecords($this->records[$betterRecordIndex], $record)) {
+
+                $previousRecord = $this->records[$betterRecordIndex];
+
+                $this->records[$betterRecordIndex] = $record;
+                $this->records[$betterRecordIndex+1] = $previousRecord;
+
+                $newPosition = $betterRecordIndex + 1;
+                $this->positionPerPlayer[$record->getPlayerLogin()] = $betterRecordIndex + 1;
+                $this->positionPerPlayer[$previousRecord->getPlayerLogin()] = $betterRecordIndex + 2;
+
+                $betterRecordIndex--;
+            }
+
+            // Remove entries whose position is superior to the limit.
+            $this->records = array_slice($this->records, 0, $this->nbRecords);
+
+            if ($newPosition <= $this->nbRecords) {
+                if ($newPosition != $oldPosition) {
+                    return [
+                        self::COL_EVENT => self::EVENT_TYPE_BETTER_POS,
+                        self::COL_RECORD => $record,
+                        self::COL_OLD_RECORD => $oldRecord,
+                        self::COL_RECORDS => $this->records,
+                        self::COL_POS => $newPosition,
+                        self::COL_OLD_POS => $oldPosition,
+                    ];
+                } else {
+                    return [
+                        self::COL_EVENT => self::EVENT_TYPE_SAME_POS,
+                        self::COL_RECORD => $record,
+                        self::COL_OLD_RECORD => $oldRecord,
+                        self::COL_RECORDS => $this->records,
+                        self::COL_POS => $newPosition,
+                    ];
+                }
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Get a new record instance.
+     *
+     * @param $login
+     *
+     * @return Record
+     */
+    protected function getNewRecord($login)
+    {
+        $record = new Record();
+        $record->setPlayerLogin($login);
+        $record->setNbLaps($this->currentNbLaps);
+        $record->setNbFinish(0);
+        $record->setMapUid($this->currentMapUid);
+
+        return $record;
+    }
+
+    /**
+     * Update Records statistics.
+     *
+     * @param Record $record
+     * @param        $score
+     */
+    protected function updateRecordStats(Record $record, $score)
+    {
+        $record->setAvgScore(
+            (($record->getAvgScore() * $record->getNbFinish()) + $score) / ($record->getNbFinish() + 1)
+        );
+        $record->setNbFinish($record->getNbFinish() + 1);
+    }
+
+
+    /**
+     * Get ordering use for sorting.
+     *
+     * @return string
+     */
+    protected function getScoreOrdering()
+    {
+        return $this->ordering;
+    }
+
+    /**
+     * @param int $newScore
+     * @param Record $record
+     *
+     * @return bool
+     */
+    protected function compareNewScore($newScore, $record)
+    {
+        if ($this->getScoreOrdering() == self::ORDER_ASC) {
+            return $newScore <= $record->getScore();
+        } else {
+            return $newScore >= $record->getScore();
+        }
+    }
+
+    /**
+     * @param Record $previousRecord
+     * @param Record $newRecord
+     *
+     * @return bool
+     */
+    protected function compareRecords(Record $previousRecord, Record $newRecord)
+    {
+        if ($this->getScoreOrdering() == self::ORDER_ASC) {
+            return $newRecord->getScore() < $previousRecord->getScore();
+        } else {
+            return $newRecord->getScore() > $previousRecord->getScore();
+        }
+    }
+}
