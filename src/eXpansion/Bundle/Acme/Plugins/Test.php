8d7071ba7168b61152b1a1d021bbd6151e1315bd
reaby <petri.jarvisalo@gmail.com>
2017-12-05 12:22:28 +0200

    hide memory widget

--- src/eXpansion/Bundle/Acme/Plugins/Test.php
+++ src/eXpansion/Bundle/Acme/Plugins/Test.php
@@ -115,7 +115,7 @@ class Test implements ListenerInterfaceExpApplication, ListenerInterfaceMpLegacy
             }
 
             self::$memoryMsg = $msg;
-            $this->mlFactory->update($this->playergroup);
+      //      $this->mlFactory->update($this->playergroup);
             $this->console->writeln($msg);
         }
 
@@ -183,7 +183,7 @@ class Test implements ListenerInterfaceExpApplication, ListenerInterfaceMpLegacy
     public function onApplicationReady()
     {
         $this->startMemValue = memory_get_usage(false);
-        $this->mlFactory->create($this->playergroup);
+     //   $this->mlFactory->create($this->playergroup);
     }
 
     /**
