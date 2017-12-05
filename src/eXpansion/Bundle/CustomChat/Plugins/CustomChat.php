857bad09ee76c5a2cc097e653fe50fdeea82a752
reaby <petri.jarvisalo@gmail.com>
2017-12-05 23:57:53 +0200

    refactor chat plugin

--- src/eXpansion/Bundle/CustomChat/Plugins/CustomChat.php
+++ src/eXpansion/Bundle/CustomChat/Plugins/CustomChat.php
@@ -68,96 +68,90 @@ class CustomChat implements ListenerInterfaceExpApplication, ListenerInterfaceMp
     public function onPlayerChat(Player $player, $text)
     {
         $text = trim($text);
-        $from = trim($player->getNickName());
+        $nick = trim($player->getNickName());
 
         if ($player->getPlayerId() == 0) {
             return;
         }
 
         if ($player->getPlayerId() != 0 && substr($text, 0, 1) != "/") {
-            $nick = $player->getNickName();
-
-            $nick = str_ireplace('$w', '', $nick);
-            $nick = str_ireplace('$z', '$z$s', $nick);
-
-            // fix for chat...
-            $nick = str_replace('$<', '', $nick);
-            $nick = str_replace('$>', '', $nick);
-
-            $text = str_replace('$<', '', $text);
             $matches = [];
 
-            $enabled = $this->enabled;
-            try {
-                $color = '$ff0';
-                $separator = '';
-                if ($this->adminGroups->isAdmin($player->getLogin())) {
-                    $color = '$ff0';
-                    $separator = '';
-                    $enabled = true;
-                }
+            if ($this->enabled || $this->adminGroups->isAdmin($player->getLogin())) {
+                $matchFound = false;
+                $matchLogin = [];
 
-                if ($enabled) {
-                    $matchFound = false;
-                    $matchLogin = [];
+                if (preg_match_all("/(\@(?P<login>[\w-._]+)\s?)/", $text, $matches)) {
+                    $group = [];
 
-                    if (preg_match_all("/(\@(?P<login>[\w-._]+)\s?)/", $text, $matches)) {
-                        $group = [];
-
+                    foreach ($matches['login'] as $login) {
                         foreach ($this->playerStorage->getOnline() as $player) {
-                            foreach ($matches['login'] as $login) {
-                                if ($player->getLogin() == $login) {
-                                    $matchFound = true;
-                                    $matchLogin[$player->getLogin()] = $player->getLogin();
-                                }
+                            if ($player->getLogin() == $login) {
+                                $matchFound = true;
+                                $matchLogin[$player->getLogin()] = $player->getLogin();
                             }
-                            $group[$player->getLogin()] = $player->getLogin();
                         }
+                        $group[$player->getLogin()] = $player->getLogin();
+                    }
 
-                        $diff = array_diff($group, $matchLogin);
+                    $diff = array_diff($group, $matchLogin);
 
-                        if ($matchFound) {
-                            $this->connection->chatSendServerMessage(
-                                '$fff$<'.$nick.'$>$z$s$fff '.$separator.' $f9f'.$text,
-                                $matchLogin
-                            );
+                    if ($matchFound) {
+                        $this->sendChat($player, $text, '$f90', $matchLogin);
 
-                            if (count($diff) > 0) {
-                                $this->connection->chatSendServerMessage(
-                                    '$fff$<'.$nick.'$>$z$s$fff '.$separator.' '.$color.$text,
-                                    $diff
-                                );
-                            }
-                            $this->console->writeln('$ff0['.$from.'$ff0] '.$text);
-
-                            return;
-                        } else {
-                            $this->connection->chatSendServerMessage(
-                                '$fff$<'.$nick.'$>$z$s$fff '.$separator.' '.$color.$text,
-                                null
-                            );
-                            $this->console->writeln('$ff0['.$from.'$ff0] '.$text);
-
-                            return;
+                        if (count($diff) > 0) {
+                            $this->sendChat($player, $text, '$ff0', $group);
                         }
+                        $this->console->writeln('$ff0['.$nick.'$ff0] '.$text);
+
+                        return;
                     } else {
-                        $this->connection->chatSendServerMessage(
-                            '$fff$<'.$nick.'$>$z$s$fff '.$separator.' '.$color.$text,
-                            null
-                        );
-                        $this->console->writeln('$ff0['.$from.'$ff0] '.$text);
+                        $this->sendChat($player, $text, '$ff0', null);
+                        $this->console->writeln('$ff0['.$nick.'$ff0] '.$text);
+
+                        return;
                     }
                 } else {
-                    $this->console->writeln('$333['.$from.'$333] '.$text);
-                    $this->chatNotification->sendMessage('expansion_customchat.chat.disabledstate',
-                        $player->getLogin());
+                    $this->sendChat($player, $text, '$ff0', null);
+                    $this->console->writeln('$ff0['.$nick.'$ff0] '.$text);
                 }
-
-            } catch (\Exception $e) {
-                $this->console->writeln('$ff0 error while sending chat: $fff'.$e->getMessage());
+            } else {
+                $this->console->writeln('$333['.$nick.'$333] '.$text);
+                $this->chatNotification->sendMessage('expansion_customchat.chat.disabledstate',
+                    $player->getLogin());
             }
         }
 
+    }
+
+    /**
+     * @param Player $player
+     * @param $text
+     * @param $color
+     * @param null $group
+     */
+    private function sendChat(Player $player, $text, $color, $group = null)
+    {
+        $nick = trim($player->getNickName());
+        $nick = str_ireplace('$w', '', $nick);
+        $nick = str_ireplace('$z', '$z$s', $nick);
+
+        // fix for chat...
+        $nick = str_replace(['$<', '$>'], '', $nick);
+        $text = str_replace(['$<', '$>'], '', $text);
+
+        $separator = '';
+        if ($this->adminGroups->isAdmin($player->getLogin())) {
+            $separator = '';
+        }
+
+        try {
+            $this->connection->chatSendServerMessage(
+                '$fff$<'.$nick.'$>$z$s$fff '.$separator.' '.$color.$text, $group
+            );
+        } catch (\Exception $e) {
+            $this->console->writeln('$ff0 error while sending chat: $fff'.$e->getMessage());
+        }
     }
 
     /**
