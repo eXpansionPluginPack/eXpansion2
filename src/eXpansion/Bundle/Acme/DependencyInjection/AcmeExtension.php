f3984cdce7d6387a882c9419421d923c73b8b802
reaby <petri.jarvisalo@gmail.com>
2017-12-09 16:47:08 +0200

    add connecting and disconnecting fake players, add saving and loading matchsettings files

--- src/eXpansion/Bundle/Acme/DependencyInjection/AcmeExtension.php
+++ src/eXpansion/Bundle/Acme/DependencyInjection/AcmeExtension.php
@@ -22,5 +22,6 @@ class AcmeExtension extends Extension
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('plugins.yml');
+        $loader->load('chat_commands.yml');
     }
 }
