d08af5de59dfac792f0617c51117bb4c316997a1
oliverde8 <oliverde8@gmail.com>
2018-01-20 17:44:00 +0100

    Improves start scripts. Fixed default database host.

--- bin/run.bat
+++ bin/run.bat
@@ -5,12 +5,12 @@ cd ..
 
 chcp 65001
 
+ECHO Clearing caches
+RMDIR /S /Q var\cache\prod
+
 ECHO Checking for database updates
 php bin/console propel:migration:diff --env=prod
 php bin/console propel:migration:migrate --env=prod
-
-ECHO Clearing caches
-php bin/console cache:clear --no-warmup --env=prod
 
 ECHO Launching eXpansion
 php bin\console eXpansion:run -vvv --env=prod
--- bin/run.sh
+++ bin/run.sh
@@ -4,13 +4,13 @@
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 cd ..
 
+echo "Clearing caches"
+rm -rf var/cache/*
+
+echo ""
 echo "Checking for database updates"
 ./bin/console propel:migration:diff --env=prod
 ./bin/console propel:migration:migrate --env=prod
-
-echo ""
-echo "Clearing caches"
-./bin/console cache:clear --no-warmup --env=prod
 
 echo ""
 echo "Launching eXpansion"
