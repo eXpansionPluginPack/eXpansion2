dba662bc5e25a354d5f6c2fb53ce5a3d2872ec87
De Cramer Oliver <oliverde8@gmail.Com>
2018-01-20 16:37:18 +0100

    Fix windows run not using prod mode for all commands

--- bin/run.bat
+++ bin/run.bat
@@ -6,8 +6,8 @@ cd ..
 chcp 65001
 
 ECHO Checking for database updates
-php bin/console propel:migration:diff
-php bin/console propel:migration:migrate
+php bin/console propel:migration:diff --env=prod
+php bin/console propel:migration:migrate --env=prod
 
 ECHO Clearing caches
 php bin/console cache:clear --no-warmup --env=prod
