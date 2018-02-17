86af0f6aa5ba75d9373c5511128a03e5f207c685
oliverde8 <oliverde8@gmail.com>
2018-02-17 19:47:51 +0100

    #27 - Added text & textarea config's

---
+++ src/eXpansion/Framework/Config/Model/TextAreaConfig.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace eXpansion\Framework\Config\Model;
+
+
+/**
+ * Class TextAreaConfig
+ *
+ * @package eXpansion\Framework\Config\Model;
+ * @author  oliver de Cramer <oliverde8@gmail.com>
+ */
+class TextAreaConfig extends TextConfig
+{
+
+}
