6a8ae25bf70c67a0c0a7d96442521e13d8811fcf
oliverde8 <oliverde8@gmail.com>
2018-02-18 17:24:23 +0100

    #262 - Allow analytics plugin to run only in prod mode.

--- src/eXpansion/Framework/Core/DependencyInjection/eXpansionCoreExtension.php
+++ src/eXpansion/Framework/Core/DependencyInjection/eXpansionCoreExtension.php
@@ -33,7 +33,6 @@ class eXpansionCoreExtension extends Extension
             $container->setParameter("expansion.config.$paramName", $value);
         }
 
-
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('commands.yml');
         $loader->load('services.yml');
@@ -45,8 +44,10 @@ class eXpansionCoreExtension extends Extension
         $loader->load('gui_grid.yml');
         $loader->load('helpers.yml');
         $loader->load('listeners.yml');
-
-        // Temporary for the prototype.
         $loader->load('plugins.yml');
+
+        if ($container->getParameter('kernel.environment') == 'prod') {
+            $loader->load('plugins_prod.yml');
+        }
     }
 }
