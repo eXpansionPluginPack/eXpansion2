abeaee2090962f50ff2632e2ba288f3e049d6a5d
oliverde8 <oliverde8@gmail.com>
2018-02-27 14:45:52 +0100

    #33 - Added fields to make admin configs possible ingame.

--- src/eXpansion/Framework/Config/Model/TextListConfig.php
+++ src/eXpansion/Framework/Config/Model/TextListConfig.php
@@ -12,6 +12,43 @@ namespace eXpansion\Framework\Config\Model;
  */
 class TextListConfig extends TextConfig
 {
+
+    /**
+     * Add a new value to the list.
+     *
+     * @param $element
+     *
+     * @return $this
+     */
+    public function add($element) {
+        $currentValue = $this->getRawValue();
+
+        if (!in_array($element, $currentValue)) {
+            $currentValue[] = $element;
+            $this->setRawValue($currentValue);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Remove an element from the list.
+     *
+     * @param $element
+     */
+    public function remove($element)
+    {
+        $currentValue = $this->getRawValue();
+
+        if (($key = array_search($element, $currentValue)) !== false) {
+            unset($currentValue[$key]);
+            $this->setRawValue($currentValue);
+        }
+    }
+
+    /**
+     * @return string
+     */
     public function __toString(): string
     {
         return implode(',', $this->get());
