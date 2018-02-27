66ad8b49b77f5b7a1329772245a32b7dc81327ad
oliverde8 <oliverde8@gmail.com>
2018-02-27 14:45:52 +0100

    #33 - Added fields to make admin configs possible ingame.

--- src/eXpansion/Framework/Config/Model/PlayerListConfig.php
+++ src/eXpansion/Framework/Config/Model/PlayerListConfig.php
@@ -5,7 +5,6 @@ namespace eXpansion\Framework\Config\Model;
 use eXpansion\Framework\PlayersBundle\Model\Player;
 use eXpansion\Framework\PlayersBundle\Storage\PlayerDb;
 
-
 /**
  * Class TextConfig
  *
@@ -18,16 +17,38 @@ class PlayerListConfig extends TextListConfig
     protected $playerDb;
 
     /**
+     * @inheritdoc
+     */
+    public function add($element)
+    {
+       if (is_object($element)) {
+           return parent::add($element->getLogin());
+       }
+
+       return parent::add($element);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function remove($element)
+    {
+        if (is_object($element)) {
+            return parent::remove($element->getLogin());
+        }
+
+        return parent::remove($element);    }
+
+
+    /**
      * Get list of configured players.
      *
      * @return Player[]
      */
     public function get()
     {
-        $list = parent::get();
         $players = [];
-
-        foreach ($list as $login) {
+        foreach (parent::get() as $login) {
             $player = $this->playerDb->get($login);
             if (is_null($player)) {
                 $player = new Player();
