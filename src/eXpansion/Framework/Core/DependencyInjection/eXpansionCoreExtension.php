db359e02033c785da729b6c4a46e5cb643d68145
Reaby <petri.jarvisalo@gmail.com>
2018-04-15 16:17:33 +0300

    fixes #261 - widget placement

--- src/eXpansion/Framework/Core/DependencyInjection/eXpansionCoreExtension.php
+++ src/eXpansion/Framework/Core/DependencyInjection/eXpansionCoreExtension.php
@@ -40,10 +40,11 @@ class eXpansionCoreExtension extends Extension implements PrependExtensionInterf
     /**
      * Loads a specific configuration.
      *
-     * @param array $configs An array of configuration values
+     * @param array            $configs An array of configuration values
      * @param ContainerBuilder $container A ContainerBuilder instance
      *
      * @throws \InvalidArgumentException When provided tag is not defined in this extension
+     * @throws \Exception
      */
     public function load(array $configs, ContainerBuilder $container)
     {
